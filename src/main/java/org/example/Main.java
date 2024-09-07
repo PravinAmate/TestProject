f40d6f3ae68ef26be021ebcc61c984eab25a1586
unknown <you@example.com>
2024-09-07 18:10:01 +0530

    add git-demo project to local repo

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,20 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+       // Write a code to convert List to map using Stream APIs
+       // Write a program - to check if one string is a rotation of another,
+         //       example 1 : s1 = "Netcracker", s2 = "ckernetcra" It should return true
+      //  example 2 : s1 = "Netcracker", s2 = "crackrenet" It should return false
+
+      // select * from Employee OrderBy salary dec limit 1,1;
+
+
+    }
+}
